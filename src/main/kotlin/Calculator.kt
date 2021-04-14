64713c9f2ebee77c2908ad10d0a2881b98701d60
Jose Antonio Rodriguez <joseantonio.rodriguez@novatec-gmbh.de>
2021-04-14 13:25:16 +0200

    Add unit test

---
+++ src/main/kotlin/Calculator.kt
@@ -0,0 +1,6 @@
+class Calculator {
+    fun add(x: Int, y: Int): Int {
+        return x + y
+    }
+
+}
---
+++ src/test/kotlin/CalculatorTest.kt
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class CalculatorTest {
+
+    @Test
+    fun `Adding three and two should return five`() {
+        val actual = Calculator().add(3,2)
+        Assertions.assertEquals(5, actual)
+    }
+}
